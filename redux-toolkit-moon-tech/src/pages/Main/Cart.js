f2ace26efff464d96a0c0e37463ad51fe0ffc9d8
ishtiyak98 <rifatishtiyak@gmail.com>
2022-12-17 19:13:22 +0600

    cart-feature

--- redux-toolkit-moon-tech/src/pages/Main/Cart.js
+++ redux-toolkit-moon-tech/src/pages/Main/Cart.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import ProductCard from "../../components/ProductCard";
 
 const Cart = () => {
-  
+  const cart = useSelector((state) => state.cart.cart);
+  console.log(cart);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      <h1>CArt</h1>
+      {cart.map((product) => (
+        <ProductCard key={product._id} product={product}></ProductCard>
+      ))}
     </div>
   );
 };
